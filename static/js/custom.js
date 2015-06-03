4fb8aea92056cd7760c424a1e61312ee190e26e3
gitliver <oe2118@cumc.columbia.edu>
2015-06-03 18:18:43 -0400

    add js code to show/hide publication highlights

--- static/js/custom.js
+++ static/js/custom.js
@@ -22,3 +22,29 @@ function useQtips()
 	    });
 	});
 }
+
+function highlightSelected()
+{
+	// flag for toggling
+	var myFlag = 0;
+	var mySpeed = 500;
+
+	$('#toggle-event').change(function() {
+		if ( myFlag == 0 ) 
+		{
+			$(".non-highlighted-pub").slideUp();
+			$(".ocustom-smallheader").slideUp();
+			/*
+			$(".non-highlighted-pub").hide();
+			$(".ocustom-smallheader").hide();
+			*/
+			myFlag = 1;
+		} 
+		else 
+		{
+			$(".ocustom-smallheader").show("slow");
+			$(".non-highlighted-pub").show("slow");
+			myFlag = 0;
+		}
+	})
+}
