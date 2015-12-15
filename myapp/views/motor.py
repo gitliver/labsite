d0047a68e434ad9d4c71d7e457523a6688a679ff
gitliver <oe2118@cumc.columbia.edu>
2015-12-15 17:24:40 -0500

    minor: fix indent issue, case issue in motor app

--- myapp/views/motor.py
+++ myapp/views/motor.py
@@ -53,65 +53,57 @@ def index():
 
 @motor.route('/_get_gene_result')
 def get_result():
-	"""JS AJAX calls this function, which queries the database with a user-submitted gene"""
+    """JS AJAX calls this function, which queries the database with a user-submitted gene"""
+ 
+    # get the user-submitted gene
+    mygene = request.args.get('mygene', "Not Found")
+    mydb = request.args.get('mydb', "No database selected")
+ 
+    # map the names of the database variable to db table name and image folder path
+    mynamemap = {'db1': ('motor2304', '/static/motorimages/images_2304/'), 'db2': ('motor', '/static/motorimages/images_440/')}
+ 
+    # gene name tuple
+    t=(mygene,)
+ 
+    # this dict contains info about the gene
+    geneinfo = {}
+ 
+    # query gene in the database
+    try:
+        # set database 
+        geneinfo['mydb'] = mydb
+ 
+        # extract info from db
+        cur = g.db.execute('select Gene, Cells, Mean, Min, Max, Connectivity, p_value, BH_p_value, Centroid, Dispersion, RNA_binding, Splicing, Surface, Transcription FROM ' + mynamemap[mydb][0] + ' WHERE Gene=?', t)
+        x = cur.fetchone()
+        geneinfo['Gene'] = x[0]
+        geneinfo['Cells'] = x[1]
+        geneinfo['Mean'] = x[2]
+        geneinfo['Min'] = x[3]
+        geneinfo['Max'] = x[4]
+        geneinfo['Connectivity'] = x[5]
+        geneinfo['p_value'] = x[6]
+        geneinfo['BH_p_value'] = x[7]
+        geneinfo['Centroid'] = x[8]
+        geneinfo['Dispersion'] = x[9]
+        geneinfo['RNA_binding'] = x[10]
+        geneinfo['Splicing'] = x[11]
+        geneinfo['Surface'] = x[12]
+        geneinfo['Transcription'] = x[13]
+ 
+        # get motor img path
+        mopath = mynamemap[mydb][1] + geneinfo['Gene'][0] + '/' + geneinfo['Gene']
+        # add img paths to geneinfo dict
+        geneinfo['img1'] = mopath + "/output1.png"
+        geneinfo['img2'] = mopath + "/output2.png"
+        geneinfo['img3'] = mopath + "/output3.png"
+        geneinfo['img4'] = mopath + "/output4.png"
+ 
+    except:
+        # if gene not in database, return this
+        geneinfo = { 'Gene': 'not found' }
 
-	# get the user-submitted gene
-	mygene = request.args.get('mygene', "Not Found")
-	mydb = request.args.get('mydb', "No database selected")
-
-	# map the names of the database variable to db table name and image folder path
-	mynamemap = {'db1': ('motor2304', '/static/motorimages/images_2304/'), 'db2': ('motor', '/static/motorimages/images_440/')}
-
-	# make the form case insensitive
-	# genes that end with the 'Rik' string, get special treatment
-	if ('rik' == mygene[-3:].lower()): 
-		mygene = mygene[:-3].upper() + 'Rik'
-	# cast the case - gene names have uppercase first letter and the rest lowercase (e.g., "Pou5f1")
-	else:
-		mygene = mygene[0].upper() + mygene[1:].lower()
-
-	# gene name tuple
-	t=(mygene,)
-
-	# this dict contains info about the gene
-	geneinfo = {}
-
-	# query gene in the database
-	try:
-		# set database 
-		geneinfo['mydb'] = mydb
-
-		# extract info from db
-		cur = g.db.execute('select Gene, Cells, Mean, Min, Max, Connectivity, p_value, BH_p_value, Centroid, Dispersion, RNA_binding, Splicing, Surface, Transcription FROM ' + mynamemap[mydb][0] + ' WHERE Gene=?', t)
-		x = cur.fetchone()
-		geneinfo['Gene'] = x[0] 
-		geneinfo['Cells'] = x[1] 
-		geneinfo['Mean'] = x[2] 
-		geneinfo['Min'] = x[3] 
-		geneinfo['Max'] = x[4] 
-		geneinfo['Connectivity'] = x[5] 
-		geneinfo['p_value'] = x[6] 
-		geneinfo['BH_p_value'] = x[7] 
-		geneinfo['Centroid'] = x[8] 
-		geneinfo['Dispersion'] = x[9] 
-		geneinfo['RNA_binding'] = x[10]
-		geneinfo['Splicing'] = x[11] 
-		geneinfo['Surface'] = x[12] 
-		geneinfo['Transcription'] = x[13]
-
-		# get motor img path
-		mopath = mynamemap[mydb][1] + geneinfo['Gene'][0] + '/' + geneinfo['Gene']
-		# add img paths to geneinfo dict
-		geneinfo['img1'] = mopath + "/output1.png"
-		geneinfo['img2'] = mopath + "/output2.png"
-		geneinfo['img3'] = mopath + "/output3.png"
-		geneinfo['img4'] = mopath + "/output4.png"
-
-	except:
-		# if gene not in database, return this
-		geneinfo = { 'Gene': 'not found' }
-
-	return jsonify(geneinfo)
+    return jsonify(geneinfo)
 
 @motor.route('/_motor_autocomplete')
 def motor_autocomplete():
